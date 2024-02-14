3098030e1471c1ba0c575d2e355fd825b2d9b62d
Dale Bustad <dale@divmain.com>
2024-02-13 23:09:21 -0800

    remove dumb mistake

--- react/src/index.js
+++ react/src/index.js
@@ -1,18 +1,5 @@
-import { isMainThread, workerData, parentPort } from 'node:worker_threads';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import Component from './Component';
 
-
 export default (remaining) => ReactDOMServer.renderToString(<Component remaining={remaining} />);
-
-if (!isMainThread) {
-  (async function () {
-    await module.exports(workerData.size);
-  })().then(
-    () => parentPort.postMessage('ok'),
-    (err) => parentPort.postMessage(err.stack),
-  );
-} else if (require.main === module) {
-  module.exports(20);
-}
